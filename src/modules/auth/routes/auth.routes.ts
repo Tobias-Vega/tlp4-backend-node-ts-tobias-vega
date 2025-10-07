5d1bd81a1ae3a63881b08dc846be732aeafaee08
Tobías Vega <tobiasvega1210@gmail.com>
2025-10-07 11:58:24 -0300

    feat: :sparkles: registro restringido a usuario admin

--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -4,6 +4,7 @@ import { validation } from "../../../middlewares/validation.middleware.js";
 import { loginValidation, registerValidation } from "../validations/auth.validation.js";
 import { AuthService } from "../services/auth.service.js";
 import { validateJWT } from "../../../middlewares/validate-jwt.middleware.js";
+import { permission } from "../../../middlewares/role.middleware.js";
 
 const authRouter = Router();
 
@@ -11,8 +12,10 @@ const authService = new AuthService();
 const userController = new UserController(authService);
 
 authRouter.post(
-  "/register", 
-  registerValidation, 
+  "/register",
+  validateJWT,
+  permission(['admin']),
+  registerValidation,
   validation, 
   userController.register
 );
